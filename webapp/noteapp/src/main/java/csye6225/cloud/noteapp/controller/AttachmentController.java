cda31cd241b2be89134b56f0e6a1c909e09648be
keyurdonga <keyurdonga123@gmail.com>
2019-02-20 18:43:44 -0500

    Delete and update working

--- webapp/noteapp/src/main/java/csye6225/cloud/noteapp/controller/AttachmentController.java
+++ webapp/noteapp/src/main/java/csye6225/cloud/noteapp/controller/AttachmentController.java
@@ -2,16 +2,18 @@ package csye6225.cloud.noteapp.controller;
 
 import com.google.gson.JsonObject;
 import csye6225.cloud.noteapp.exception.AppException;
+import csye6225.cloud.noteapp.model.Attachment;
+import csye6225.cloud.noteapp.model.Notes;
 import csye6225.cloud.noteapp.repository.AttachmentRepository;
 import csye6225.cloud.noteapp.service.AttachmentService;
+import csye6225.cloud.noteapp.service.NotesService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
 
 @RestController
 public class AttachmentController {
@@ -22,8 +24,11 @@ public class AttachmentController {
     @Autowired
     private AttachmentService as;
 
+    @Autowired
+    private NotesService ns;
+
     @PostMapping("/note/{noteid}/attachment")
-    public ResponseEntity<Object> singleFileUpload(@RequestParam("file") MultipartFile file, Authentication auth, @PathVariable final String noteid) throws AppException {
+    public ResponseEntity<Object> addAttachments(@RequestParam("file") MultipartFile file, Authentication auth, @PathVariable final String noteid) throws AppException {
 
         if (file.isEmpty()) {
             JsonObject entity = new JsonObject();
@@ -41,6 +46,91 @@ public class AttachmentController {
             entity.addProperty("Error", "Access denied.");
             return ResponseEntity.status(401).body(entity.toString());
         }
+    }
+
+    @PutMapping("/note/{noteid}/attachment/{attachmentid}")
+    public ResponseEntity<Object> updateAttachments(@RequestParam("file") MultipartFile file, Authentication auth, @PathVariable final String noteid, @PathVariable final String attachmentid) throws AppException {
+
+        if (file.isEmpty()) {
+            JsonObject entity = new JsonObject();
+            entity.addProperty("Error","Please attach one file.");
+            return ResponseEntity.badRequest().body(entity.toString());
+        }
+        Notes note = ns.findNotesById(noteid);
+        if (note == null) {
+            JsonObject entity = new JsonObject();
+            entity.addProperty("Error","Note not found. Please enter a valid note id.");
+            return ResponseEntity.badRequest().body(entity.toString());
+        }
+        if(auth.getName().equalsIgnoreCase(note.getUser_id())){
+            /*int present = 0;
+            for (Attachment a:note.getAttachments()) {
+                if(a.getAttachment_id().equalsIgnoreCase(attachmentid)){
+                    present = 1;
+                    as.updateAttachment(file,note,a);
+                }
+            }
+            if(present != 1){
+                JsonObject entity = new JsonObject();
+                entity.addProperty("Error", "Attachment not found. Please enter a valid attachment ID");
+                return ResponseEntity.ok().body(entity.toString());
+            }else{
+                JsonObject entity = new JsonObject();
+                entity.addProperty("Success", "Updated the attachment.");
+                return ResponseEntity.status(204).body(entity.toString());
+            }*/
+            String status = as.updateAttachment(file,note,attachmentid);
+            if(status == null){
+                JsonObject entity = new JsonObject();
+                entity.addProperty("Error", "Attachment not found. Please enter a valid attachment ID");
+                return ResponseEntity.ok().body(entity.toString());
+            }else{
+                JsonObject entity = new JsonObject();
+                entity.addProperty("Success", "Updated the attachment.");
+                return ResponseEntity.status(204).body(entity.toString());
+            }
+        }else{
+            JsonObject entity = new JsonObject();
+            entity.addProperty("Error", "Access denied.");
+            return ResponseEntity.status(401).body(entity.toString());
+        }
+
+
+    }
+
+    @DeleteMapping("/note/{noteid}/attachment/{attachmentid}")
+    public ResponseEntity<Object> deleteAttachments(Authentication auth, @PathVariable final String noteid, @PathVariable final String attachmentid) throws AppException {
+
+        if(noteid == null || attachmentid == null){
+            JsonObject entity = new JsonObject();
+            entity.addProperty("Error", "Please enter a valid note & attachment id");
+            return ResponseEntity.badRequest().body(entity.toString());
+        }
+        Notes note = ns.findNotesById(noteid);
+        if (note == null) {
+            JsonObject entity = new JsonObject();
+            entity.addProperty("Error","Note not found. Please enter a valid note id");
+            return ResponseEntity.badRequest().body(entity.toString());
+        }
+        if(auth.getName().equalsIgnoreCase(note.getUser_id())){
+            int present = as.deleteAttachment(note,attachmentid);
+            if(present != 1){
+                JsonObject entity = new JsonObject();
+                entity.addProperty("Error", "Attachment not found. Please enter a valid attachment ID");
+                return ResponseEntity.ok().body(entity.toString());
+            }else{
+                JsonObject entity = new JsonObject();
+                entity.addProperty("Success", "Deleted the attachment.");
+                return ResponseEntity.status(204).body(entity.toString());
+            }
+
+        }else{
+            JsonObject entity = new JsonObject();
+            entity.addProperty("Error", "Access denied.");
+            return ResponseEntity.status(401).body(entity.toString());
+        }
+
+
     }
 
 }
--- webapp/noteapp/src/main/java/csye6225/cloud/noteapp/service/AttachmentService.java
+++ webapp/noteapp/src/main/java/csye6225/cloud/noteapp/service/AttachmentService.java
@@ -11,11 +11,14 @@ import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
 
 @Component
@@ -31,14 +34,14 @@ public class AttachmentService {
 
     @Autowired
     private Environment environment;
+
     @Autowired
     private NotesRepository noteRepository;
 
     public String createAttachment(MultipartFile file, String name, String noteid) throws AppException {
         try {
             Notes nt = notesService.findNotesById(noteid);
-            if (nt.getUser_id().equalsIgnoreCase(name)) {
-                System.out.println(environment);
+            if (nt != null && nt.getUser_id().equalsIgnoreCase(name)) {
                 Attachment attachment = new Attachment();
                 UUID uuid = UUID.randomUUID();
 
@@ -46,7 +49,7 @@ public class AttachmentService {
                 String type = mimeType.split("/")[1];
 
                 byte[] bytes = file.getBytes();
-                Path path = Paths.get(local_storage + "/" + file.getOriginalFilename());
+                Path path = Paths.get(local_storage + "/"  + uuid.toString() + "-" + file.getOriginalFilename());
                 Files.write(path, bytes);
                 attachment.setAttachment_id(uuid.toString());
                 attachment.setPath(path.toString());
@@ -60,5 +63,50 @@ public class AttachmentService {
         catch (IOException exc){
             throw new AppException("IOException");
         }
+    }
+
+    public String updateAttachment(MultipartFile file, Notes note, String attachmentid) throws AppException {
+        try {
+            for (Attachment a:note.getAttachments()) {
+                if(a.getAttachment_id().equalsIgnoreCase(attachmentid)) {
+                    String mimeType = file.getContentType();
+                    File oldfile = new File(a.getPath());
+                    if (oldfile.exists()) {
+                        oldfile.delete();
+                    }
+
+                    byte[] bytes = file.getBytes();
+                    Path path = Paths.get(local_storage + "/" + a.getAttachment_id() + "-" + file.getOriginalFilename());
+                    Files.write(path, bytes);
+                    a.setPath(path.toString());
+                    noteRepository.save(note);
+                    return "Success";
+                }
+            }
+            return null;
+        }
+        catch (IOException exc){
+            throw new AppException("IOException");
+        }
+    }
+
+    public int deleteAttachment(Notes note, String attachmentid) {
+        Attachment attach = null;
+        for (Attachment a:note.getAttachments()) {
+            if(a.getAttachment_id().equalsIgnoreCase(attachmentid)){
+                File oldfile = new File(a.getPath());
+                if (oldfile.exists()) {
+                    oldfile.delete();
+                }
+                attach = a;
+
+            }
+        }
+        if(attach != null){
+            note.getAttachments().remove(attach);
+            noteRepository.save(note);
+            return 1;
+        }
+        return 0;
     }
 }

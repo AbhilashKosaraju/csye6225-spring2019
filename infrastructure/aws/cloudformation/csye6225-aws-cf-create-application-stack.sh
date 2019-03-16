7d02fa8db6cb3f6738f47e5d2d2d3a472b6a48eb
keyurdonga <keyurdonga123@gmail.com>
2019-03-16 19:33:25 -0400

    changes to application stack

--- infrastructure/aws/cloudformation/csye6225-aws-cf-create-application-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-create-application-stack.sh
@@ -45,18 +45,11 @@ fi
 
 
 
-vpcID=$(aws ec2 describe-vpcs --query 'Vpcs[].{VpcId:VpcId}' \
+VpcId=$(aws ec2 describe-vpcs --query 'Vpcs[].{VpcId:VpcId}' \
 --filters "Name=tag:Name,Values=$networkStackName-csye6225-vpc" "Name=is-default, Values=false" --output text 2>&1)
 
 
-if [ -z "$vpcID" ]
-then
-	echo "VPC ID error \n Exiting"
-	exit 1
-fi
-
-
-subnet=$(aws ec2 describe-subnets --filters Name=vpc-id,Values=${vpcID})
+subnet=$(aws ec2 describe-subnets --filters Name=vpc-id,Values=${VpcId})
 subnetid1=$(echo -e "$subnet" | jq '.Subnets[0].SubnetId' | tr -d '"')
 subnetid2=$(echo -e "$subnet" | jq '.Subnets[1].SubnetId' | tr -d '"')
 subnetid3=$(echo -e "$subnet" | jq '.Subnets[2].SubnetId' | tr -d '"')
@@ -70,6 +63,11 @@ then
 fi
 
 
+if [ -z "$VpcId" ]
+then
+	echo "VPC ID error \n Exiting"
+	exit 1
+fi
 
 
 if [ -z "$subnetid2" ]
@@ -85,6 +83,20 @@ then
 	exit 1
 fi
 
+
+
+# Create CloudFormation Stack
+echo "Validating template"
+TMP_code=`aws cloudformation validate-template --template-body file://./csye6225-cf-application.json`
+if [ -z "$TMP_code" ]
+then
+	echo "Template error exiting! "
+	exit 1
+fi
+echo "Cloudformation template validation success"
+
+echo "Now Creating CloudFormation Stack"
+
 export circleciuser=circleci
 
 DOMAIN_NAME=$(aws route53 list-hosted-zones --query HostedZones[0].Name --output text)
@@ -94,21 +106,8 @@ DOMAIN_NAME1=$(aws route53 list-hosted-zones --query HostedZones[0].Name --outpu
 
 CD_DOMAIN="code-deploy."${DOMAIN_NAME1%?}
 
-# Create CloudFormation Stack
-echo "Validating template"
-TMP_code=`aws cloudformation validate-template --template-body file://./csye6225-cf-application.json`
-if [ -z "$TMP_code" ]
-then
-	echo "Template error exiting!"
-	exit 1
-fi
-echo "Cloudformation template validation success"
-
-echo "Now Creating CloudFormation Stack"
-
-
-CRTSTACK_Code=`aws cloudformation create-stack --stack-name $appStackName --template-body file://./csye6225-cf-application.json --capabilities CAPABILITY_NAMED_IAM --parameters   ParameterKey=KeyName,ParameterValue=$keyName ParameterKey=myVpc,ParameterValue=$vpcID ParameterKey=circleci,ParameterValue=$circleciuser ParameterKey=PublicSubnetKey1,ParameterValue=$subnetid1 ParameterKey=PublicSubnetKey2,ParameterValue=$subnetid2 ParameterKey=PublicSubnetKey3,ParameterValue=$subnetid3  ParameterKey=ImageID,ParameterValue=$imageid ParameterKey=Bucket,ParameterValue=arn:aws:s3:::$Bucket ParameterKey=Bucket1,ParameterValue=arn:aws:s3:::$Bucket/* ParameterKey=CDARN,ParameterValue=arn:aws:s3:::$CD_DOMAIN ParameterKey=CDARN1,ParameterValue=arn:aws:s3:::$CD_DOMAIN/ ParameterKey=Bucket3,ParameterValue=$Bucket`
-
+CRTSTACK_Code=`aws cloudformation create-stack --stack-name $appStackName --template-body file://./csye6225-cf-application.json --capabilities CAPABILITY_NAMED_IAM --parameters ParameterKey=NetworkStackNameParameter,ParameterValue=$networkStackName ParameterKey=ApplicationStackNameParameter,ParameterValue=$appStackName  ParameterKey=KeyName,ParameterValue=$keyName ParameterKey=VpcID,ParameterValue=$VpcId ParameterKey=PublicSubnetKey1,ParameterValue=$subnetid1 ParameterKey=PublicSubnetKey2,ParameterValue=$subnetid2 ParameterKey=PublicSubnetKey3,ParameterValue=$subnetid3 ParameterKey=ImageID,ParameterValue=$imageid ParameterKey=circleci,ParameterValue=$circleciuser ParameterKey=Bucket,ParameterValue=arn:aws:s3:::$Bucket ParameterKey=Bucket1,ParameterValue=arn:aws:s3:::$Bucket/* ParameterKey=CDARN,ParameterValue=arn:aws:s3:::$CD_DOMAIN ParameterKey=CDARN1,ParameterValue=arn:aws:s3:::$CD_DOMAIN/* ParameterKey=S3Bucket,ParameterValue=$Bucket`
+ 
 if [ -z "$CRTSTACK_Code" ]
 then
 	echo "Stack Creation error exiting!"

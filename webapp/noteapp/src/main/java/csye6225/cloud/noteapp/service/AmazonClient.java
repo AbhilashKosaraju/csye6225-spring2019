924ad5680228245bd456a117b6647566128866ae
Vrushali Patil <patil.vr@husky.neu.edu>
2019-03-16 17:55:30 -0400

    Updates

--- webapp/noteapp/src/main/java/csye6225/cloud/noteapp/service/AmazonClient.java
+++ webapp/noteapp/src/main/java/csye6225/cloud/noteapp/service/AmazonClient.java
@@ -37,11 +37,11 @@ public class AmazonClient {
     @Value("${spring.amazonProperties.bucketName}")
     private String bucketName;
     @Value("${spring.datasource.url}")
-    private static String rdsUrl;
+    private String rdsUrl;
     @Value("${spring.datasource.username}")
-    private static String username;
+    private String username;
     @Value("${spring.datasource.password}")
-    private static String password;
+    private String password;
 
     @PostConstruct
     private void initializeAmazon() {
@@ -90,17 +90,13 @@ public class AmazonClient {
         return "Successfully deleted";
     }
 
-    public static Connection getRemoteConnection() {
+    public Connection getRemoteConnection() {
         if (System.getenv("RDS_HOSTNAME") != null) {
             try {
                 Class.forName("com.mysql.cj.jdbc.Driver");
                 String jdbcUrl = rdsUrl + "?user=" + username + "&password=" + password;
                 Connection con = DriverManager.getConnection(jdbcUrl);
                 return con;
-            }
-            catch (ClassNotFoundException e) {
-                 }
-            catch (SQLException e) {
             }
         }
         return null;

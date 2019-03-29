6ca3ab08d2af4c9b03e7f1abfc6fd713825144c6
Vrushali Patil <patil.vr@husky.neu.edu>
2019-03-29 02:02:10 -0400

    Updated policies

--- infrastructure/aws/cloudformation/applicationstack/csye6225-aws-cf-create-application-stack.sh
+++ infrastructure/aws/cloudformation/applicationstack/csye6225-aws-cf-create-application-stack.sh
@@ -94,6 +94,9 @@ DOMAIN_NAME1=$(aws route53 list-hosted-zones --query HostedZones[0].Name --outpu
 
 CD_DOMAIN="code-deploy."${DOMAIN_NAME1%?}
 
+CDSR=$(aws iam get-role --role-name CodeDeployServiceRole --query "Role.Arn" --output text)
+
+
 # Create CloudFormation Stack
 echo "Validating template"
 TMP_code=`aws cloudformation validate-template --template-body file://./csye6225-cf-application.json`
@@ -107,7 +110,7 @@ echo "Cloudformation template validation success"
 echo "Now Creating CloudFormation Stack"
 
 
-CRTSTACK_Code=`aws cloudformation create-stack --stack-name $appStackName --template-body file://./csye6225-cf-application.json --capabilities CAPABILITY_NAMED_IAM --parameters   ParameterKey=KeyName,ParameterValue=$keyName ParameterKey=myVpc,ParameterValue=$vpcID ParameterKey=circleci,ParameterValue=$circleciuser ParameterKey=PublicSubnetKey1,ParameterValue=$subnetid1 ParameterKey=PublicSubnetKey2,ParameterValue=$subnetid2 ParameterKey=PublicSubnetKey3,ParameterValue=$subnetid3  ParameterKey=ImageID,ParameterValue=$imageid ParameterKey=Bucket,ParameterValue=arn:aws:s3:::$Bucket ParameterKey=Bucket1,ParameterValue=arn:aws:s3:::$Bucket/* ParameterKey=CDARN,ParameterValue=arn:aws:s3:::$CD_DOMAIN ParameterKey=CDARN1,ParameterValue=arn:aws:s3:::$CD_DOMAIN/* ParameterKey=Bucket3,ParameterValue=$Bucket ParameterKey=cdm,ParameterValue=$CD_DOMAIN`
+CRTSTACK_Code=`aws cloudformation create-stack --stack-name $appStackName --template-body file://./csye6225-cf-application.json --capabilities CAPABILITY_NAMED_IAM --parameters   ParameterKey=KeyName,ParameterValue=$keyName ParameterKey=myVpc,ParameterValue=$vpcID ParameterKey=circleci,ParameterValue=$circleciuser ParameterKey=PublicSubnetKey1,ParameterValue=$subnetid1 ParameterKey=PublicSubnetKey2,ParameterValue=$subnetid2 ParameterKey=PublicSubnetKey3,ParameterValue=$subnetid3  ParameterKey=ImageID,ParameterValue=$imageid ParameterKey=Bucket,ParameterValue=arn:aws:s3:::$Bucket ParameterKey=Bucket1,ParameterValue=arn:aws:s3:::$Bucket/* ParameterKey=CDARN,ParameterValue=arn:aws:s3:::$CD_DOMAIN ParameterKey=CDARN1,ParameterValue=arn:aws:s3:::$CD_DOMAIN/* ParameterKey=Bucket3,ParameterValue=$Bucket ParameterKey=cdm,ParameterValue=$CD_DOMAIN ParameterKey=CDR,ParameterValue=$CDSR`
 
 if [ -z "$CRTSTACK_Code" ]
 then

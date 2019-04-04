ff2f2acb79f4019ed2cba11c88fc9a93a42572a1
keyurdonga <keyurdonga123@gmail.com>
2019-04-04 15:32:50 -0400

    change

--- infrastructure/aws/codedeploy/afterInstall.sh
+++ infrastructure/aws/codedeploy/afterInstall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo systemctl stop tomcat.service
+#sudo systemctl stop tomcat.service
 
 sudo rm -rf /opt/tomcat/webapps/docs  /opt/tomcat/webapps/examples /opt/tomcat/webapps/host-manager  /opt/tomcat/webapps/manager /opt/tomcat/webapps/ROOT* /opt/tomcat/webapps/noteapp-1
 
@@ -13,5 +13,6 @@ sudo rm -rf /opt/tomcat/logs/catalina*
 sudo rm -rf /opt/tomcat/logs/*.log
 sudo rm -rf /opt/tomcat/logs/*.txt
 
+sudo systemctl restart tomcat.service
 
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/opt/tomcat/cloudwatch-config.json -s

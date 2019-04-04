ea4b6e75ea0c509822637e9092519d3dcd575d05
keyurdonga <keyurdonga123@gmail.com>
2019-04-04 04:35:23 -0400

    changes to appspec

--- infrastructure/aws/codedeploy/applicationStart.sh
+++ infrastructure/aws/codedeploy/applicationStart.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+until [ -f /opt/tomcat/demofile.txt ]; do
+	echo "waiting for ec2 instance"
+	sleep 5
+done
+echo "ec2 instance is running in code deploy"
+
 # start tomcat service
 #sudo systemctl start tomcat.service
 cd /home/centos
